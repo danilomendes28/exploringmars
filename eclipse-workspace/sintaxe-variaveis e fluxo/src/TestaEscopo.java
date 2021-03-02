946cd56b42904f004c9c1441ee5269afc50574b0
Danilo <danilo2803@icloud.com>
2021-03-02 14:04:04 -0300

    Final desenvolvimento

---
+++ eclipse-workspace/sintaxe-variaveis e fluxo/src/TestaEscopo.java
@@ -0,0 +1,23 @@
+
+public class TestaEscopo {
+	public static void main(Strings[] args) {
+		System.out.println("testando condicionais");
+		
+		int idade =20;
+		int quantidadePessoas =3;
+		
+		boolean acompanhado = quantidadePessoas >= 2;
+		
+			
+		
+		System.out.println("valor de acompanhado =" + acompanhado);
+	
+			if (idade >= 18 && acompanhado) {
+				System.out.println("seja bem vindo");
+		
+			}
+			
+	}
+	
+
+}
